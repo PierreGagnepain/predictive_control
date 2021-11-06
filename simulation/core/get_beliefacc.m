39599587404b40e4de5ea88c992b63aed0ae24d1
Pierre Gagnepain <gagnepain@cyceron.fr>
2021-11-06 08:53:52 +0100

    cleaning belief accuracy function

--- simulation/core/get_beliefacc.m
+++ simulation/core/get_beliefacc.m
@@ -1,16 +1,6 @@
-function [ACC,BIC,AIC] = get_beliefacc(belief,nu,y,opt_idx)
+function [ACC,BIC,AIC] = get_beliefacc(belief,y,opt_idx)
 
-% beta function
-betaprobdensity = @(alpha,beta,x) gamma(alpha+beta)./(gamma(alpha).*gamma(beta)).*x.^(alpha-1).*(1-x).^(beta-1);
-
-alpha           = belief*nu;
-beta            = nu - alpha;
-
-
-% Negative log-model accuracy of previous response (see tapas optim
-% function)
-% y           = 0.95.*(y-0.5)+0.5;% shrink y
-% negLogLl    = -nansum(log(betaprobdensity(alpha,beta,y)));
+% Negative log-model accuracy of previous response (see tapas optim function)
 negLogLl    = -nansum(y.*log(belief) + (1-y).*log(1-belief));
 ACC         = -negLogLl;
 
